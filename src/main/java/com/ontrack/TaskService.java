a28915d2668057f6a086d771a6d5957ceddf32a8
Dmatt547 <Danielmattioli2005@gmail.com>
2025-05-17 14:55:06 +1000

    Gettaskdetauls returns null

--- src/main/java/com/ontrack/TaskService.java
+++ src/main/java/com/ontrack/TaskService.java
@@ -2,17 +2,21 @@ package com.ontrack;
 
 public class TaskService {
     
+//	public Task getTaskDetails(String taskId) {
+//        // You can expand this if needed, but hardcoded for test
+//        if ("9.1P".equals(taskId)) {
+//            return new Task(
+//                "9.1P",
+//                "TDD and CI",
+//                "2025-05-21",
+//                "https://ontrack.deakin.edu.au/tasksheets/tdd-ci.pdf"
+//            );
+//        }
+//        return null;
+//    }
+	
 	public Task getTaskDetails(String taskId) {
-        // You can expand this if needed, but hardcoded for test
-        if ("9.1P".equals(taskId)) {
-            return new Task(
-                "9.1P",
-                "TDD and CI",
-                "2025-05-21",
-                "https://ontrack.deakin.edu.au/tasksheets/tdd-ci.pdf"
-            );
-        }
-        return null;
-    }
+	    return null;
+	}
 
 }
